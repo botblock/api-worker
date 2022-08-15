0d0424e6a37a7dc1e8f3e5ce8ffbc476622e5b8f
A5rocks <git@helvetica.moe>
2022-08-15 14:40:17 +0900

    Drop schemas

--- src/util/getList.js
+++ src/util/getList.js
@@ -9,14 +9,21 @@ module.exports = id => {
     const list = { ...match };
 
     // Load full features
-    list.features = featuresData.map(feature => ({
-        ...feature,
-        value: list.features.includes(feature.id) ? 1 : 0,
-    })).sort((a, b) => {
+    list.features = featuresData.map(feature => {
+        delete feature['$schema'];
+
+        return {
+            ...feature,
+            value: list.features.includes(feature.id) ? 1 : 0,
+        };
+    }).sort((a, b) => {
         if (a.value !== b.value) return a.value ? -1 : 1;
         if (a.display !== b.display) return a.display > b.display ? -1 : 1;
         return a.name.localeCompare(b.name) ? -1 : 1;
     });
+
+    // Drop $schema
+    delete list['$schema'];
 
     return list;
 };
